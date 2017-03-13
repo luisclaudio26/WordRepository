620c93e2589d1f990cf1d043e1ff2a0b42480c02
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2017-03-13 16:52:57 -0300

    first commit

---
+++ words/repo/IRepository.java
@@ -0,0 +1,8 @@
+package words.repo;
+
+import java.rmi.*;
+
+public interface IRepository extends java.rmi.Remote
+{
+	
+}
---
+++ words/repo/Repository.java
@@ -0,0 +1,17 @@
+package words.repo;
+
+import java.rmi.*;
+import java.rmi.server.*;
+
+public class Repository extends UnicastRemoteObject implements IRepository
+{
+	public Repository() throws RemoteException
+	{
+
+	}
+
+	public static void main(String[] args)
+	{
+
+	}
+}
