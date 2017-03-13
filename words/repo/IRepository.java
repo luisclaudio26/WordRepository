eefdc1a9a4ee0815f48cbb10db16d53f7f757dd5
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2017-03-13 20:28:21 -0300

    first working Repository. Not tested yet!

--- words/repo/IRepository.java
+++ words/repo/IRepository.java
@@ -4,5 +4,7 @@ import java.rmi.*;
 
 public interface IRepository extends java.rmi.Remote
 {
-	
+	void pushWord(String w) throws RemoteException;
+	void removeWord(String w) throws RemoteException;
+	boolean hasWord(String w) throws RemoteException;
 }
--- words/repo/Repository.java
+++ words/repo/Repository.java
@@ -2,16 +2,43 @@ package words.repo;
 
 import java.rmi.*;
 import java.rmi.server.*;
+import java.util.Set;
+import java.util.TreeSet;
 
-public class Repository extends UnicastRemoteObject implements IRepository
-{
-	public Repository() throws RemoteException
-	{
+public class Repository extends UnicastRemoteObject implements IRepository {
+	Set<String> words;
 
+	public Repository() throws RemoteException {
+		words = new TreeSet<String>();
 	}
 
-	public static void main(String[] args)
-	{
+	//------------------------------
+	//------ From IRepository ------
+	//------------------------------
+	@Override
+	public void pushWord(String w) {
+		words.add(w);
+	}
 
+	@Override
+	public void removeWord(String w) {
+		words.remove(w);
+	}
+
+	@Override
+	public boolean hasWord(String w) {
+		return words.contains(w);
+	}
+
+	//----- repository thread -----
+	public static void main(String[] args) {
+		try {
+			String host = "localhost";
+			IRepository rep = new Repository();
+			Naming.rebind("//localhost/WordRep", rep);
+		} catch(Exception e) {
+			System.err.println("REPOSITORY ERROR:\n" + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
